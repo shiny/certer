c3dd8be49a3a7cfa91526aff3a39aff66121fc60
daijie <daijie@keqin.tech>
2022-12-11 20:35:49 +0000

    fix: cert's expire date, not order's

--- app/Models/Cert.ts
+++ app/Models/Cert.ts
@@ -2,6 +2,7 @@ import { DateTime } from 'luxon'
 import { BaseModel, column, hasOne, HasOne } from '@ioc:Adonis/Lucid/Orm'
 import CertOrder from './CertOrder'
 import { Order, Ca, createEcdsaCsr } from 'handyacme'
+import { X509Certificate } from "node:crypto"
 type Alg = "ECDSA" | "RSA"
 export function isAlg(alg: string): alg is Alg {
   return ["ECDSA", "RSA" ].includes(alg)
@@ -70,14 +71,12 @@ export default class Cert extends BaseModel {
     cert.csr = csr
     cert.key = privateKey
     cert.alg = "ECDSA"
-    cert.expiredAt = order.expiredAt
     return cert
   }
 
   async finalize(acmeClient: Ca) {
     const order = await CertOrder.findOrFail(this.certOrderId)
     const acmeOrder = await Order.restore(acmeClient, order.orderUrl)
-    // order.certificateUrl = 
     const result = await acmeOrder.finalize(this.csr)
     return result
   }
@@ -86,6 +85,9 @@ export default class Cert extends BaseModel {
     const order = await CertOrder.findOrFail(this.certOrderId)
     const acmeOrder = new Order(acmeClient)
     const certContent = await acmeOrder.downloadCertification(order.certificateUrl)
+    const x509cert = new X509Certificate(certContent)
+    const validTo = new Date(x509cert.validTo)
+    this.expiredAt = DateTime.fromJSDate(validTo)
     this.cert = certContent
   }
 
